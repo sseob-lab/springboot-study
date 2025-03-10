deedb39985476b09375eeec8cc682becdd5d5c9e
*Wdrz1Adqmy <shim_yosub@mads.co.jp>
2025-03-10 18:27:26 +0900

    DDD Layer로패키지변경

--- src/main/java/com/study/springboot/developer/Member/Domain/Repositories/MemberRepository.java
+++ src/main/java/com/study/springboot/developer/Member/Domain/Repositories/MemberRepository.java
@@ -1,9 +1,9 @@
-package com.study.springboot.developer;
+package com.study.springboot.developer.Member.Domain.Repositories;
 
+import com.study.springboot.developer.Member.Domain.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
 }
