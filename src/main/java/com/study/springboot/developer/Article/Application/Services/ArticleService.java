746cb6311b72afcfc5d5e63dcc51f3a8ca970206
ToBeSD <kuzoteki@gmail.com>
2025-03-21 13:14:55 +0900

    글보기기능추가

--- src/main/java/com/study/springboot/developer/Article/Application/Services/ArticleService.java
+++ src/main/java/com/study/springboot/developer/Article/Application/Services/ArticleService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ArticleService {
@@ -14,5 +15,9 @@ public class ArticleService {
 
     public List<Article> getAllArticles() {
         return articleRepository.findAll();
+    }
+
+    public Optional<Article> findById(Long id) {
+        return articleRepository.findById(id);
     }
 }
--- src/main/java/com/study/springboot/developer/Article/UserInterface/Controller/ArticleController.java
+++ src/main/java/com/study/springboot/developer/Article/UserInterface/Controller/ArticleController.java
@@ -1,25 +1,44 @@
 package com.study.springboot.developer.Article.UserInterface.Controller;
 
 import com.study.springboot.developer.Article.Application.Dto.Response.ArticleListViewResponse;
+import com.study.springboot.developer.Article.Application.Dto.Response.ArticleViewResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.study.springboot.developer.Article.Application.Services.ArticleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired
     ArticleService articleService;
 
     @GetMapping("/articles")
-    public String articleList() {
+    public String articleList(Model model) throws JsonProcessingException {
         List<ArticleListViewResponse> articles = articleService.getAllArticles()
             .stream()
             .map(ArticleListViewResponse::new)
             .toList();
 
+        model.addAttribute("articles", articles);
+
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String article(Model model, @PathVariable Long id) {
+        ArticleViewResponse article = articleService.findById(id)
+            .map(ArticleViewResponse::new)
+            .orElseThrow();
+
+        model.addAttribute("article", article);
+
+        return "article";
     }
 }
