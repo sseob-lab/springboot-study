fb6ef64b7c3117dcc5bd2a76501a32d2ade8ddd4
ToBeSD <kuzoteki@gmail.com>
2025-03-21 13:02:07 +0900

    글목록보기기능추가

--- src/main/java/com/study/springboot/developer/Article/UserInterface/Controller/ArticleController.java
+++ src/main/java/com/study/springboot/developer/Article/UserInterface/Controller/ArticleController.java
@@ -1,17 +1,25 @@
 package com.study.springboot.developer.Article.UserInterface.Controller;
 
+import com.study.springboot.developer.Article.Application.Dto.Response.ArticleListViewResponse;
 import com.study.springboot.developer.Article.Application.Services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ArticleController {
     @Autowired
-    ArticleService testService;
+    ArticleService articleService;
 
     @GetMapping("/articles")
     public String articleList() {
+        List<ArticleListViewResponse> articles = articleService.getAllArticles()
+            .stream()
+            .map(ArticleListViewResponse::new)
+            .toList();
+
         return "articleList";
     }
 }
